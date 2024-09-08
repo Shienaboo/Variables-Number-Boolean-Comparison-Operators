79600dcd042bc8df56239a4d122d8df9095a4a1a
unknown <nshienarocell@gmail.com>
2024-09-08 06:24:58 -0800

    Completed Variables Section

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("Hello, World!");
+let myname = "Shiena Rocell Naungayan";
+console.log('My name is ' + myname);
+
+//Variables
+let name = "Shiena Rocell Naungayan";
+let age = "19";
+console.log("Hello! I am " + name + age + " years old.");
+
+//Numbers
+const a = 4;
+const b = 9;
+let sum = a + b;
+console.log(sum);
+
+//Booleans
+let c = true;
+let d = false;
+console.log("I am learning programming", c);
+console.log("I will give up", d);
+
+//Comparison-Operators
+let e = 20;
+let f = 25;
+
+let iseGreaterThanf = e > f;
+let iseLessThanf = e < f;
+let iseEqualTof = e == f;
+let iseNotEqualTof = e !== f;
+
+console.log("Is e greater than f?", iseGreaterThanf);
+console.log("Is e less than f?", iseLessThanf);
+console.log("Is e equal to f?", iseEqualTof);
+console.log("Is e not equal to f?", iseNotEqualTof);
